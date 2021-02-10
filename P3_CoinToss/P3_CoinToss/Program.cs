675087de9fd1db95074204efb1b252e080ff2b53
Laura Goon <Laura.goon@ou.edu>
2021-02-09 18:22:02 -0600

    started Coin Toss Participation

---
+++ P3_CoinToss/P3_CoinToss/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P3_CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 3);// generate a random number between 1 and 2
+
+            Console.WriteLine(randomNumber);
+
+        }
+    }
+}
