264689ab5505821855c1cf78ebae5c4bae21a7a4
Laura Goon <Laura.goon@ou.edu>
2021-02-11 18:28:58 -0600

    Asked for heads or tails and that's all

--- P3_CoinToss/P3_CoinToss/Program.cs
+++ P3_CoinToss/P3_CoinToss/Program.cs
@@ -6,11 +6,31 @@ namespace P3_CoinToss
     {
         static void Main(string[] args)
         {
+            // 1 = heads
+            // 2 = tails
+            Console.WriteLine("Heads or Tails?");
+            string answer = Console.ReadLine();
+
+            // ask the user for heads or tails
+
             Random rand = new Random();
             int randomNumber = rand.Next(1, 3);// generate a random number between 1 and 2
-
             Console.WriteLine(randomNumber);
+            // generate random number (done above)
 
+            if (randomNumber == 1);
+            {
+                Console.WriteLine("The answer is heads!");
+            }
+            else
+            {
+                Console.WriteLine("The answer is tails!");
+            }
+            string Heads = Convert.ToString(randomNumber);
+            // convert either the user's answer to an int or our random number to a string
+
+
+            //see if they guessed right with the newly converted value 
         }
     }
 }
